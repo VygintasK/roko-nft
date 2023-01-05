e9cc0249276b62a7dc35f5b9e612ab01b48079c2
Vyga <vygintaskirda@gmail.com>
2023-01-05 09:49:32 +0200

    Subscribe Section full

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import logo from '../../Img/logo.png'
+import './Footer.css'
 import { ReactComponent as DiscordLogo } from "./DiscordLogo.svg"
 import { ReactComponent as InstagramLogo } from "./InstagramLogo.svg"
 import { ReactComponent as TwitterLogo } from "./TwitterLogo.svg"
@@ -9,27 +10,27 @@ const Footer = () => {
   return (
     <footer>
       <div className='footerInfo'>
-        <div>
-          <img src={logo}></img>
+        <div className='market'>
+          <img alt='logo' src={logo}></img>
           <p>NFT marketplace UI created with Anima for Figma.</p>
           <p>Join our community</p>
-          <div>
+          <div className='social-networks-wrap'>
             <DiscordLogo/>
             <YoutubeLogo/>
             <TwitterLogo/>
             <InstagramLogo/>
           </div>
         </div>
-        <div>
-          <p>Explore</p>
+        <div className='explore'>
+          <h5>Explore</h5>
           <ul>
             <li>Marketplace</li>
             <li>Rankings</li>
             <li>Connect a wallet</li>
           </ul>
         </div>
-        <div>
-          <p>Join our weekly digest</p>
+        <div className='join'>
+          <h5>Join our weekly digest</h5>
           <p>Get exclusive promotions & updates straight to your inbox.</p>
           <input></input>
         </div>
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroSection from "./HeroSection/heroSection"
+import SubscribeWidget from "./SubscribeWidget/SubscribeWidget"
 
 const Home = () => {
   return (
     <>
      <HeroSection/>  
+     <SubscribeWidget/>  
     </>
   )
 }
