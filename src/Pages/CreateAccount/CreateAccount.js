9aa81167e57119936f00b91037c6c1fd23214204
Vyga <vygintaskirda@gmail.com>
2023-01-11 13:22:50 +0200

    Create account page

--- src/Pages/CreateAccount/CreateAccount.js
+++ src/Pages/CreateAccount/CreateAccount.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import './CreateAccount.css'
+
+import CreateAccountPic from './CreateAccount.png'
 
 const CreateAccount = () => {
   return (
-    <div>CreateAccount</div>
+    <div className='CreateAccount'>
+      <img src={CreateAccountPic}></img>
+      <div>
+        <h2>Create account</h2>
+        <p>Welcome! enter your details and start creating, collecting and selling NFTs.</p>
+        <form className='create-account-form'>
+          <input type='text'/>
+          <input type='email'/>
+          <input type='text'/>
+          <input type='text'/>
+          <Link className='create-account-button'>Create account</Link>
+        </form>
+      </div>
+
+    </div>
   )
 }
 
